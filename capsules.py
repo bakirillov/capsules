7279778e6707f3aa65096c96626f72a9a226e4b5
bakirillov <bakirillov@localhost.localdomain>
2018-10-22 10:32:30 +0300

    Now it is working correctly

--- capsules.py
+++ capsules.py
@@ -6,19 +6,17 @@ from torch.autograd import Variable
 
 def squash(vector, dim=-1):
     """Activation function for capsule"""
-    sj2 = (vector**2).sum(-1, keepdim=True)
+    sj2 = (vector**2).sum(dim, keepdim=True)
     sj = sj2 ** 0.5
     return(
         sj2/(1.0+sj2)*vector/sj
     )
 
-def find_probability(t):
-    """Compute softmaxed length of vector"""
-    return(F.softmax((t.squeeze(-1)**2).sum(-1)**0.5, dim=-1))
-
-def get_a_capsule(u,n,k):
-    """Return the contents of a capsule"""
-    return(u.squeeze(-1)[n,k,:])
+def make_y(labels, n_classes):
+    masked = Variable(torch.eye(n_classes))
+    masked = masked.cuda() if torch.cuda.is_available() else masked
+    masked = masked.index_select(dim=0, index=labels)
+    return(masked)
 
 
 class PrimaryCapsuleLayer(nn.Module):
@@ -34,8 +32,7 @@ class PrimaryCapsuleLayer(nn.Module):
         )
     
     def __init__(
-        self, n_capsules=8, n_routes=32*6*6, in_ch=256, 
-        out_ch=32, kernel_size=9, stride=2, cuda=True
+        self, n_capsules=8, in_ch=256, out_ch=32, kernel_size=9, stride=2
     ):
         super(PrimaryCapsuleLayer, self).__init__()
         self.n_capsules = n_capsules
@@ -44,22 +41,19 @@ class PrimaryCapsuleLayer(nn.Module):
         self.kernel_size = kernel_size
         self.stride = stride
         self.n_capsules = n_capsules
-        self.n_routes = n_routes
         self.capsules = nn.ModuleList([])
         for a in range(self.n_capsules):
             self.capsules.append(self.make_conv())
             
     def forward(self, x):
         """Compute outputs of capsules, reshape and squash"""
-        out = torch.stack([a(x) for a in self.capsules], dim=1).view(
-            x.size(0), self.n_routes, -1
-        )
+        out = torch.cat([a(x).view(x.size(0), -1, 1) for a in self.capsules], dim=-1)
         return(squash(out))
-    
+        
     
 class SecondaryCapsuleLayer(nn.Module):
     
-    def __init__(self, n_capsules=10, n_iter=3, n_routes=32*6*6, in_ch=8, out_ch=16, cuda=True):
+    def __init__(self, n_capsules=10, n_iter=3, n_routes=32*6*6, in_ch=8, out_ch=16):
         super(SecondaryCapsuleLayer, self).__init__()
         self.n_capsules = n_capsules
         self.n_iter = n_iter
@@ -67,29 +61,19 @@ class SecondaryCapsuleLayer(nn.Module):
         self.in_ch = in_ch
         self.out_ch = out_ch
         self.W = nn.Parameter(
-            torch.randn(1, self.n_routes, self.n_capsules, self.out_ch, self.in_ch)
+            torch.randn(self.n_capsules, self.n_routes, self.in_ch, self.out_ch)
         )
-        self.cuda = cuda
     
     def forward(self, x):
-        """Perform routing by agreement"""
-        batch_size = x.size(0)
-        u_hat = torch.matmul(
-            torch.cat([self.W]*x.size(0), dim=0),
-            torch.stack([x]*self.n_capsules, dim=2).unsqueeze(4)
-        )
-        b_ij = Variable(torch.zeros(1, self.n_routes, self.n_capsules, 1))
-        b_ij = b_ij.cuda() if self.cuda else b_ij
-        for a in range(self.n_iter):
-            c_ij = torch.cat(
-                [F.softmax(b_ij, dim=1)]*x.size(0), dim=0
-            ).unsqueeze(4)
-            s_j = (c_ij*u_hat).sum(dim=1, keepdim=True)
-            v_j = squash(s_j)
-            if a < self.n_iter - 1:
-                a_ij = torch.matmul(u_hat.transpose(3, 4), torch.cat([v_j]*self.n_routes, dim=1))
-                b_ij = b_ij + a_ij.squeeze(4).mean(dim=0, keepdim=True)
-        return(v_j.squeeze(1))
+        P = x[None, :, :, None, :] @ self.W[:, None, :, :, :]
+        L = Variable(torch.zeros(*P.size()))
+        L = L.cuda() if torch.cuda.is_available() else L
+        for i in range(self.n_iter):
+            probabilities = F.softmax(L, dim=2)
+            out = squash((probabilities*P).sum(dim=2, keepdim=True))
+            if i != self.n_iter - 1:
+                L = L + (P*out).sum(dim=-1, keepdim=True)
+        return(out)
     
     
 class RegularizingDecoder(nn.Module):
@@ -109,33 +93,20 @@ class RegularizingDecoder(nn.Module):
         """Feed-forward reconstructor"""
         return(self.decoder(x))
     
-
+    
 class CapsuleLoss(nn.Module):
     
-    def __init__(self, ms=(0.9, 0.1), l=0.5, adjustment=0.0005, n_classes=10, cuda=True):
+    def __init__(self, ms=(0.9, 0.1), l=0.5, adjustment=0.0005):#, n_classes=10):
         super(CapsuleLoss, self).__init__()
-        self.ms = ms
         self.l = l
         self.m_p = ms[0]
         self.m_n = ms[1]
         self.a = adjustment
-        self.n = n_classes
-        self.cudap = cuda
-        self.reconstruction_loss = nn.MSELoss()
-        self.ones = torch.eye(self.n)
-        if self.cudap:
-            self.ones = self.ones.cuda()
-        
-    def margin_loss(self, x, mask):
-        a = mask*F.relu(self.m_p-find_probability(x))
-        b = self.l*(1-mask)*F.relu(find_probability(x)-self.m_n)
-        return((a+b).sum(dim=1).mean())
-        
-    def forward(self, internal, inp, real_classes, reconstructions):
-        """Compute margin and reconstruction losses"""
-        mask = self.ones.index_select(0, real_classes)
-        ml = self.margin_loss(internal, mask)
-        rl = self.reconstruction_loss(
-            reconstructions, inp
-        )
-        return(ml+self.a*rl)
+        self.reconstruction_loss = nn.MSELoss(size_average=False)
+            
+    def forward(self, labels, inputs, classes, reconstructions):
+        left = F.relu(self.m_p - classes, inplace=True) ** 2
+        right = F.relu(classes - self.m_n, inplace=True) ** 2
+        margin_loss = (labels*left + self.l*(1-labels)*right).sum()
+        reconstruction_loss = self.reconstruction_loss(reconstructions, inputs)
+        return((margin_loss+self.a*reconstruction_loss)/inputs.size(0))
